1c2aea701c911012bfe89b9cb7a1462a1528ebc2
pettan0818 <pettan0818+git@gmail.com>
2019-01-02 22:53:28 +0900

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import Clock from '@/components/Clock'
+
+export default {
+  components: {
+    Clock
+  }
+}
+</script>
+
+<template>
+    <div>
+        <Clock class='clock'/>
+    </div>
+</template>
+
+<style scoped>
+.clock {
+    width: 100%;
+    max-width: 500px;
+    margin: 30px auto;
+}
+</style>
+<style>
+html {
+    font-size: 62.5%;
+}
+
+body {
+    margin: 0;
+}
+
+p{
+    margin:0;
+}
+</style>
